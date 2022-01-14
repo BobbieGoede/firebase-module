091633e695c164b170e5e04a46e900f5899d536e
Bobbie Goede <bobbiegoede@gmail.com>
2022-01-14 15:28:29 +0100

    fix: resolve custom env

--- packages/firebase-module/lib/module.js
+++ packages/firebase-module/lib/module.js
@@ -20,6 +20,7 @@ module.exports = function (moduleOptions) {
     (nuxt.options.publicRuntimeConfig &&
       nuxt.options.publicRuntimeConfig.fireEnv) ||
     ''
+  Object.assign(runtimeConfig, fireEnv ? runtimeConfig[fireEnv] : runtimeConfig)
   const options = Object.assign(
     defaultOptions,
     this.options.firebase,
